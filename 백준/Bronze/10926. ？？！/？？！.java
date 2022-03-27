d1a8682ed11ece3deb4b01a75a16bcfc9dccafa4
Lithium07z <100747149+Lithium07z@users.noreply.github.com>
2022-03-27 17:43:07 +0900

    [Bronze V] Title: ??!, Time: 204 ms, Memory: 17684 KB -BaekjoonHub

---
+++ 백준/Bronze/10926. ？？！/？？！.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        String ip = input.next();
+        System.out.print(ip + "??!");
+    }
+}
