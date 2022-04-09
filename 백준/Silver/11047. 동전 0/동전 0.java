f1cc0019189f0f8d00a128f2627db40b2ba7f7ff
Lithium <100747149+Lithium07z@users.noreply.github.com>
2022-04-09 13:28:27 +0900

    Update 동전 0.java

--- 백준/Silver/11047. 동전 0/동전 0.java
+++ 백준/Silver/11047. 동전 0/동전 0.java
@@ -8,7 +8,6 @@ import java.util.StringTokenizer;
 public class Main {
 	public static void main(String[] args) throws IOException {
 		BufferedReader bfr = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bfw = new BufferedWriter(new OutputStreamWriter(System.out));
 		StringTokenizer st = new StringTokenizer(bfr.readLine());
 		
 		int[] arr = new int[Integer.parseInt(st.nextToken())];
@@ -24,9 +23,8 @@ public class Main {
 		BufferedWriter bfw = new BufferedWriter(new OutputStreamWriter(System.out));
 		int count = 0;
 		int fixedValue = value; // 나눠야 할 값을 저장할 변수
-		int fixedsize = size - 1; // 사이즈를 저장할 변수 
 		
-		for (int i = fixedsize; i >= 0; i--) {
+		for (int i = size - 1; i >= 0; i--) {
 			if (value % arr[i] == value) { // 나누려는 돈이 더 큰 경우
 				continue; // 스킵
 			} else { // 입력값과 같은 자릿수의 돈인 경우
@@ -35,7 +33,7 @@ public class Main {
 			}
 		}
 		if (value != 0) { // value가 0이 아니면 잘못된 값으로 나눈 것이므로 
-			calc(fixedsize, fixedValue, arr); // 한단계 아래 크기의 돈으로 다시 나눠봄 
+			calc(size - 1, fixedValue, arr); // 한단계 아래 크기의 돈으로 다시 나눠봄 
 		} else {
 			bfw.write(String.valueOf(count)); // 아니면 출력 
 			bfw.flush();
