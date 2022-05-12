492bf4ac41502c351fddb8ee800d9b6368ad6fa1
Lithium <100747149+Lithium07z@users.noreply.github.com>
2022-05-12 20:15:41 +0900

    [Bronze II] Title: A+B - 6, Time: 128 ms, Memory: 14264 KB -BaekjoonHub

---
+++ 백준/Bronze/10953. A＋B － 6/A＋B － 6.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader bfr = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bfw = new BufferedWriter(new OutputStreamWriter(System.out));
+		int loop = Integer.parseInt(bfr.readLine());
+		
+		for (int i = 0; i < loop; i++) {
+			StringTokenizer st = new StringTokenizer(bfr.readLine(), ",");
+			bfw.write(String.valueOf(Integer.parseInt(st.nextToken()) + Integer.parseInt(st.nextToken())) + "\n");
+		}
+		bfw.flush();
+		bfw.close();
+	}
+}
