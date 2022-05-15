7b3982421e857cd709d1297cd29f2ad509f44a3f
Lithium <100747149+Lithium07z@users.noreply.github.com>
2022-05-15 23:41:27 +0900

    [Bronze III] Title: 세 수, Time: 128 ms, Memory: 14352 KB -BaekjoonHub

---
+++ 백준/Bronze/10817. 세 수/세 수.java
@@ -0,0 +1,24 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader bfr = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bfw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringTokenizer st = new StringTokenizer(bfr.readLine());
+		int arr[] = new int[3];
+		
+		for (int i = 0; i < 3; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
+		Arrays.sort(arr);
+		bfw.write(String.valueOf(arr[1]));
+		bfw.flush();
+		bfw.close();
+	}
+}
