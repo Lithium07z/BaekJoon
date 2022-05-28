9b9ac585a14d0c43d8b3506bd3a995ef8753b0a4
Lithium <100747149+Lithium07z@users.noreply.github.com>
2022-05-28 20:32:01 +0900

    [Bronze V] Title: 등록, Time: 120 ms, Memory: 14088 KB -BaekjoonHub

---
+++ 백준/Bronze/7287. 등록/등록.java
@@ -0,0 +1,16 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader bfr = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bfw = new BufferedWriter(new OutputStreamWriter(System.out));
+		bfw.write("121\n");
+        bfw.write("lithium07");
+        bfw.flush();
+        bfw.close();
+	}
+}
