2d5c6e2e250e3113936fcc096a498b057c735499
Lithium <100747149+Lithium07z@users.noreply.github.com>
2022-08-17 12:50:22 +0900

    [Unrated] Title: 생일 출력하기, Time: 72 ms, Memory: 11348 KB -BaekjoonHub

---
+++ 백준/Unrated/2555. 생일 출력하기/생일 출력하기.java
@@ -0,0 +1,15 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader bfr = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bfw = new BufferedWriter(new OutputStreamWriter(System.out));
+		bfw.write("3/19");
+		bfw.flush();
+		bfw.close();
+	}
+}
