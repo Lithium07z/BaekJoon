7066a1618ea94b8765a860cdde321209a8e43bab
Lithium <100747149+Lithium07z@users.noreply.github.com>
2022-08-20 12:49:51 +0900

    [Silver V] Title: 돌 게임 2, Time: 156 ms, Memory: 11608 KB -BaekjoonHub

---
+++ 백준/Silver/9656. 돌 게임 2/돌 게임 2.java
@@ -0,0 +1,20 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader bfr = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bfw = new BufferedWriter(new OutputStreamWriter(System.out));
+ 		
+		if (Integer.parseInt(bfr.readLine()) % 2 == 0) {
+			bfw.write("SK");
+		} else {
+			bfw.write("CY");
+		}
+		bfw.flush();
+		bfw.close();
+	}
+}
