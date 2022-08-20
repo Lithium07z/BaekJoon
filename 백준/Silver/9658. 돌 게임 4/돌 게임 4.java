662b176dfe932dc30fb96e9f64ba73a793f05c1c
Lithium <100747149+Lithium07z@users.noreply.github.com>
2022-08-20 17:19:09 +0900

    [Silver II] Title: 돌 게임 4, Time: 84 ms, Memory: 11596 KB -BaekjoonHub

---
+++ 백준/Silver/9658. 돌 게임 4/돌 게임 4.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader bfr = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bfw = new BufferedWriter(new OutputStreamWriter(System.out));
+ 		
+		int num = Integer.parseInt(bfr.readLine());
+		if (num % 7 == 1 || num % 7 == 3) {
+			bfw.write("CY");
+		} else {
+			bfw.write("SK");
+		}
+		bfw.flush();
+		bfw.close();
+	}
+}
