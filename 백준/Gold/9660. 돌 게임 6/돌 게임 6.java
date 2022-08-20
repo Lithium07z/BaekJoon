b0f80b03f0efca89c4ffd8cb650367e8085ef266
Lithium <100747149+Lithium07z@users.noreply.github.com>
2022-08-20 17:07:09 +0900

    [Gold V] Title: 돌 게임 6, Time: 76 ms, Memory: 11592 KB -BaekjoonHub

---
+++ 백준/Gold/9660. 돌 게임 6/돌 게임 6.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader bfr = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bfw = new BufferedWriter(new OutputStreamWriter(System.out));
+ 		
+		long num = Long.parseLong(bfr.readLine());
+		if (num % 7 == 0 || num % 7 == 2) {
+			bfw.write("CY");
+		} else {
+			bfw.write("SK");
+		}
+		bfw.flush();
+		bfw.close();
+	}
+}
