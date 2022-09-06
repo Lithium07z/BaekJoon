5750de0dd815f03a27bc6dba85e9fd337330251a
Lithium <100747149+Lithium07z@users.noreply.github.com>
2022-09-06 16:38:06 +0900

    [Silver III] Title: N과 M (3), Time: 604 ms, Memory: 279984 KB -BaekjoonHub

--- 백준/Silver/15651. N과 M （3）/N과 M （3）.java
+++ 백준/Silver/15651. N과 M （3）/N과 M （3）.java
@@ -9,15 +9,16 @@ import java.util.StringTokenizer;
 public class Main {
 	static BufferedWriter bfw = new BufferedWriter(new OutputStreamWriter(System.out));
 	static LinkedList<Integer> list = new LinkedList<Integer>();
+	static StringBuilder sb = new StringBuilder();
 	public static void main(String[] args) throws IOException {
 		BufferedReader bfr = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(bfr.readLine());
-		
 		
 		int N = Integer.parseInt(st.nextToken());
 		int M = Integer.parseInt(st.nextToken());
 		
 		dfs(N, M);
+		bfw.write(String.valueOf(sb));
 		bfw.flush();
 		bfw.close();
 	}
@@ -25,9 +26,9 @@ public class Main {
 	public static void dfs(int N, int M) throws IOException {
 		if (list.size() == M) {
 			for (int i = 0; i < list.size(); i++) {
-				bfw.write(String.valueOf(list.get(i)) + " ");
+				sb.append(list.get(i)).append(" ");
 			}
-			bfw.write("\n");
+			sb.append("\n");
 			return;
 		} else {
 			for (int i = 1; i < N + 1; i++) {
