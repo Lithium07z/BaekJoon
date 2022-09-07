40181c4e95c23b55f3d5fd50a3b3f4cf1ebc0519
Lithium <100747149+Lithium07z@users.noreply.github.com>
2022-09-08 08:53:17 +0900

    [Silver III] Title: N과 M (4), Time: 2040 ms, Memory: 298304 KB -BaekjoonHub

---
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.java
@@ -0,0 +1,51 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.LinkedList;
+import java.util.StringTokenizer;
+
+public class Main {
+	static LinkedList<Integer> list = new LinkedList<Integer>();
+	static StringBuilder sb = new StringBuilder();
+	public static void main(String[] args) throws IOException {
+		BufferedReader bfr = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bfw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringTokenizer st = new StringTokenizer(bfr.readLine());
+		
+		
+		int N = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
+		
+		dfs(N, M);
+		System.out.println(sb);
+	}
+	
+	public static void dfs(int N, int M) {
+		int check = -1;
+		if (list.size() == M) {
+			for (int i = 0; i < list.size() - 1; i++) {
+				for (int j = i + 1; j < list.size(); j++) {
+					if (list.get(i) > list.get(j)) {
+						check++;
+						break;
+					}
+				}
+			}
+			if (check == -1) {
+				for (int i = 0; i < list.size(); i++) {
+					sb.append(list.get(i)).append(" ");
+				}
+				sb.append("\n");
+			}
+			return;
+		} else {
+			for (int i = 1; i < N + 1; i++) {
+					list.add(i);
+					dfs(N, M);
+					list.removeLast();
+			}
+		}
+	}
+}
