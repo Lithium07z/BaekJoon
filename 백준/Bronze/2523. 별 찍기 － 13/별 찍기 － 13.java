798081fdf168f0b98321d222e258105719f44d06
Lithium <100747149+Lithium07z@users.noreply.github.com>
2022-09-09 17:24:47 +0900

    [Bronze III] Title: 별 찍기 - 13, Time: 80 ms, Memory: 11640 KB -BaekjoonHub

---
+++ 백준/Bronze/2523. 별 찍기 － 13/별 찍기 － 13.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader bfr = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		
+		int height = Integer.parseInt(bfr.readLine());
+		
+		for (int i = 1; i <= height; i++) {
+			for (int j = 0; j < i; j++) {
+				sb.append("*");
+			}
+			sb.append("\n");
+		}
+		for (int i = 1; i <= height - 1; i++) {
+			for (int j = 0; j < height - i; j++) {
+				sb.append("*");
+			}
+			sb.append("\n");
+		}
+		System.out.println(sb);
+	}
+}
