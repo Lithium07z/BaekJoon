135c895bb37cb5b97aab67a80f71fb11775fd10f
Lithium <100747149+Lithium07z@users.noreply.github.com>
2022-09-15 18:18:55 +0900

    [Bronze V] Title: 긴자리 계산, Time: 92 ms, Memory: 12128 KB -BaekjoonHub

--- 백준/Bronze/2338. 긴자리 계산/긴자리 계산.java
+++ 백준/Bronze/2338. 긴자리 계산/긴자리 계산.java
@@ -1,20 +1,19 @@
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
 import java.math.BigInteger;
 
 public class Main {
 	public static void main(String[] args) throws IOException {
 		BufferedReader bfr = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bfw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringBuilder sb = new StringBuilder();
 		
 		BigInteger bigNum1 = new BigInteger(bfr.readLine());
 		BigInteger bigNum2 = new BigInteger(bfr.readLine());
 		
-		System.out.println(bigNum1.add(bigNum2));
-		System.out.println(bigNum1.subtract(bigNum2));
-		System.out.println(bigNum1.multiply(bigNum2));
+		sb.append(bigNum1.add(bigNum2)).append("\n");
+		sb.append(bigNum1.subtract(bigNum2)).append("\n");
+		sb.append(bigNum1.multiply(bigNum2));
+		System.out.println(sb);
 	}
 }
