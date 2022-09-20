c9c939764b613ed4bab216721cb40a385fe30331
Lithium <100747149+Lithium07z@users.noreply.github.com>
2022-09-20 20:31:01 +0900

    [Bronze V] Title: 단어 길이 재기, Time: 80 ms, Memory: 11572 KB -BaekjoonHub

---
+++ 백준/Bronze/2743. 단어 길이 재기/단어 길이 재기.java
@@ -0,0 +1,15 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader bfr = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bfw = new BufferedWriter(new OutputStreamWriter(System.out));
+		bfw.write(String.valueOf(bfr.readLine().length()));
+		bfw.flush();
+		bfw.close();
+	}
+}
