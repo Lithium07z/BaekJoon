ad3a6902c705928dfe921a7a7ea50b97957e5c84
Lithium <100747149+Lithium07z@users.noreply.github.com>
2022-10-24 20:13:32 +0900

    [Gold V] Title: 나무 위의 빗물, Time: 552 ms, Memory: 119588 KB -BaekjoonHub

---
+++ 백준/Gold/17073. 나무 위의 빗물/나무 위의 빗물.java
@@ -0,0 +1,32 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Main{
+	public static void main(String args[]) throws IOException {
+		BufferedReader bfr = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bfw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringTokenizer st = new StringTokenizer(bfr.readLine());
+		int count = Integer.parseInt(st.nextToken());
+		double water = Double.parseDouble(st.nextToken());
+		int arr[] = new int[count + 1];
+		double leaf = 0;
+		
+		for (int i = 0; i < count - 1; i++) {
+			st = new StringTokenizer(bfr.readLine());
+			arr[Integer.parseInt(st.nextToken())] += 1;
+			arr[Integer.parseInt(st.nextToken())] += 1;
+		}
+		
+		for (int i = 0; i < arr.length; i++) {
+			if (i != 1 && arr[i] == 1) {
+				leaf += 1;
+			}
+		}
+		
+		System.out.printf("%.10f", water / leaf);
+	}
+}
