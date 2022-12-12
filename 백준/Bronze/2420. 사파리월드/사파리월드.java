fd6a36372a68a88d5d23d8376b66fc202e722fef
Lithium <100747149+Lithium07z@users.noreply.github.com>
2022-12-12 23:50:03 +0900

    [Bronze V] Title: 사파리월드, Time: 120 ms, Memory: 14096 KB -BaekjoonHub

---
+++ 백준/Bronze/2420. 사파리월드/사파리월드.java
@@ -0,0 +1,17 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String args[]) throws IOException {
+		BufferedReader bfr = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bfw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringTokenizer st = new StringTokenizer(bfr.readLine());
+		bfw.write(String.valueOf(Math.abs(Long.parseLong(st.nextToken()) - Long.parseLong(st.nextToken()))));
+		bfw.flush();
+		bfw.close();
+	}
+}
