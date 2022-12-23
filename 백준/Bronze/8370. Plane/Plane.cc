0cab334991c08f9191a0fd5cbd54d73b6b4fb708
Lithium <100747149+Lithium07z@users.noreply.github.com>
2022-12-23 09:59:39 +0900

    [Bronze V] Title: Plane, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/8370. Plane/Plane.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <array>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+    ios_base :: sync_with_stdio(false);
+    cin.tie(NULL);
+    short a;
+    short b;
+    short c;
+    short d;
+    cin >> a >> b >> c >> d;
+    cout << a * b + c * d;
+}
