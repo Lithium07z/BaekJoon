18e0ebf1000d9b86552b9ec27c2db23bdfc55398
Lithium <100747149+Lithium07z@users.noreply.github.com>
2023-01-01 11:10:49 +0900

    [Silver III] Title: 2×n 타일링, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/11726. 2×n 타일링/2×n 타일링.cc
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    ios_base :: sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    
+    int n;
+    cin >> n;
+    int* arr = new int[n];
+    arr[0] = 1;
+    arr[1] = 2;
+
+    for (int i = 2; i < n; i++) {
+        arr[i] = ((arr[i - 1] % 10007) + (arr[i - 2] % 10007)) % 10007;
+    }
+    cout << arr[n - 1];
+}
