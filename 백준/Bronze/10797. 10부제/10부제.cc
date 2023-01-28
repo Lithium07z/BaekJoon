14a6f2ea1cbf50168f149cba1545f4d48766b057
Lithium <100747149+Lithium07z@users.noreply.github.com>
2023-01-28 22:28:07 +0900

    [Bronze IV] Title: 10부제, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10797. 10부제/10부제.cc
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+ 
+	int n, t, c = 0;
+	cin >> n;
+
+	for (int i = 0; i < 5; i++) {
+		cin >> t;
+		if (n == t) {
+			c++;
+		}
+	}
+	cout << c;
+}
