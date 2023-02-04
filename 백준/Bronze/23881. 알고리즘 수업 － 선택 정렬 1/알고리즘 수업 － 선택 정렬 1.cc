6e8f57cb3ea1ce6ee2561de51dee9b305323dc22
Lithium <100747149+Lithium07z@users.noreply.github.com>
2023-02-04 14:29:54 +0900

    [Bronze I] Title: 알고리즘 수업 - 선택 정렬 1, Time: 160 ms, Memory: 2180 KB -BaekjoonHub

---
+++ 백준/Bronze/23881. 알고리즘 수업 － 선택 정렬 1/알고리즘 수업 － 선택 정렬 1.cc
@@ -0,0 +1,44 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+
+	int N, K, c = 0;
+	cin >> N >> K;
+
+	int *a = new int[N];
+
+	for (int i = 0; i < N; i++)
+	{
+		cin >> a[i];
+	}
+
+	for (int i = N - 1; i >= 0; i--)
+	{
+		int m = 0;
+		for (int j = 0; j <= i; j++)
+		{
+			if (a[m] < a[j])
+			{
+				m = j;
+			}
+		}
+		if (i != m)
+		{
+			c++;
+			if (c == K) {
+				cout << a[i] << " " << a[m];
+				return 0;
+			}
+			int t = a[i];
+			a[i] = a[m];
+			a[m] = t;
+		}
+	}
+	cout << -1;
+}
