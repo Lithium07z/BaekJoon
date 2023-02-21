9e2f97ec346b996e13b226d39b4a34438d9a6034
Lithium <100747149+Lithium07z@users.noreply.github.com>
2023-02-21 19:02:29 +0900

    [Silver II] Title: N번째 큰 수, Time: 904 ms, Memory: 277060 KB -BaekjoonHub

---
+++ 백준/Silver/2075. N번째 큰 수/N번째 큰 수.java
@@ -0,0 +1,32 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Collections;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		PriorityQueue<Integer> pq = new PriorityQueue<Integer>(Collections.reverseOrder());
+		StringTokenizer st;
+		int N = Integer.parseInt(br.readLine());
+		
+		for (int i = 0; i < N; i++) {
+			st = new StringTokenizer(br.readLine());
+			for (int j = 0; j < N; j++) {
+				pq.add(Integer.parseInt(st.nextToken()));
+			}
+		}
+		
+		for (int i = 0; i < N - 1; i++) {
+			pq.poll();
+		}
+		bw.write(pq.poll() + "\n");
+		bw.flush();
+		bw.close();
+	}
+}
