5ddbd7951d7bb5cd1c19539acaacdc85407050da
Lithium <100747149+Lithium07z@users.noreply.github.com>
2023-03-24 22:07:24 +0900

    [Bronze I] Title: 알고리즘 수업 - 버블 정렬 1, Time: 412 ms, Memory: 17440 KB -BaekjoonHub

--- 백준/Bronze/23968. 알고리즘 수업 － 버블 정렬 1/알고리즘 수업 － 버블 정렬 1.java
+++ 백준/Bronze/23968. 알고리즘 수업 － 버블 정렬 1/알고리즘 수업 － 버블 정렬 1.java
@@ -10,54 +10,33 @@ public class Main {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		int N = Integer.parseInt(st.nextToken());
-		int K = Integer.parseInt(st.nextToken());
-		int A[] = new int[N];
-		int idx[] = new int[N - 1];
-
+		int arr[] = new int[Integer.parseInt(st.nextToken())];
+		int chk = Integer.parseInt(st.nextToken());
+		int cnt= 0;
+		
 		st = new StringTokenizer(br.readLine());
-		for (int i = 0; i < N; i++) {
-			A[i] = Integer.parseInt(st.nextToken());
+		for (int i = 0; i < arr.length; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
 		}
 		
-		for (int i = 0; i < N - 1; i++) {
-			idx[i] = i;
-		}
-		
-		if (N * (N - 1) / 2 < K) {
-			bw.write("-1");
-			bw.flush();
-			bw.close();
-			return;
-		}
-		
-		while (idx.length > 0) {
-			int tempIdx[] = new int[idx.length];
-			int p = 0;
-			
-			for (int idxP : idx) {
-				if (A[idxP] > A[idxP + 1]) {
-					int temp = A[idxP];
-					A[idxP] = A[idxP + 1];
-					A[idxP + 1] = temp;
-					if (--K == 0) {
-						bw.write(A[idxP] + " " + A[idxP + 1]);
+		for (int i = 1; i < arr.length; i++) {
+			for (int j = 0; j < arr.length - 1; j++) {
+				if (arr[j] > arr[j + 1]) {
+					if (++cnt == chk) {
+						bw.write(arr[j + 1] + " " + arr[j]);
 						bw.flush();
 						bw.close();
 						return;
 					}
-					if (idxP > 0) {
-						tempIdx[p++] = idxP - 1;
-					}
+					int temp = arr[j];
+					arr[j] = arr[j + 1];
+					arr[j + 1] = temp;
 				}
 			}
-			
-			idx = new int[p];
-			for (int i = 0; i < p; i++) {
-				idx[i] = tempIdx[i];
-			}
 		}
-		bw.write("-1");
+		if (cnt != chk) {
+			bw.write("-1");
+		}
 		bw.flush();
 		bw.close();
 	}
