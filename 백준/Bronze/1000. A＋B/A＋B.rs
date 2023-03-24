f440bba0899be29de113b28c71ad0dc74db1f57e
Lithium <100747149+Lithium07z@users.noreply.github.com>
2023-03-24 18:44:55 +0900

    [Bronze V] Title: A+B, Time: 4 ms, Memory: 13148 KB -BaekjoonHub

---
+++ 백준/Bronze/1000. A＋B/A＋B.rs
@@ -0,0 +1,15 @@
+use std::io::{stdin, Read};
+use std::fmt::Write;
+
+fn main() {
+	let mut input = String::new();
+    stdin().read_to_string(&mut input).unwrap();
+    let mut input = input.split_ascii_whitespace().flat_map(str::parse::<usize>);
+    let mut output = String::new();
+    
+    let a = input.next().unwrap();
+    let b = input.next().unwrap();
+
+    writeln!(output, "{}", a + b).unwrap();
+    println!("{output}");
+}
