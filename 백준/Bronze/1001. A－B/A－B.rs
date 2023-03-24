0c0b72df6faa77a32eb07e42ad727e3a6edb26c0
Lithium <100747149+Lithium07z@users.noreply.github.com>
2023-03-24 19:28:07 +0900

    [Bronze V] Title: A-B, Time: 4 ms, Memory: 13152 KB -BaekjoonHub

--- 백준/Bronze/1001. A－B/A－B.rs
+++ 백준/Bronze/1001. A－B/A－B.rs
@@ -6,10 +6,7 @@ fn main() {
     let mut output = String::new();
     stdin().read_to_string(&mut input).unwrap();
     let mut input = input.split_ascii_whitespace();
-    
-    let a = input.next().unwrap().parse::<isize>().unwrap();
-    let b = input.next().unwrap().parse::<isize>().unwrap();
 
-    writeln!(output, "{}", a - b).unwrap();
+    writeln!(output, "{}", input.next().unwrap().parse::<isize>().unwrap() - input.next().unwrap().parse::<isize>().unwrap()).unwrap();
     println!("{output}");
 }
