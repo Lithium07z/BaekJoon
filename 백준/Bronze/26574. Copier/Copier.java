73788f586baf0203b924e83eaaf4e4966688432b
Lithium <100747149+Lithium07z@users.noreply.github.com>
2023-05-20 22:19:41 +0900

    [Bronze V] Title: Copier, Time: 144 ms, Memory: 16020 KB -BaekjoonHub

---
+++ 백준/Bronze/26574. Copier/Copier.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		int N = Integer.parseInt(br.readLine());
+		
+		for (int i = 0; i < N; i++) {
+			String temp = br.readLine();
+			bw.write(temp + " " + temp + "\n");
+		}
+		bw.flush();
+		bw.close();
+	}
+}
