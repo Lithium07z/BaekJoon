44fc3ea47568d063d875dd7124a8092a47d5d520
Lithium <100747149+Lithium07z@users.noreply.github.com>
2023-08-08 15:28:50 +0900

    [Gold IV] Title: 네트워크 연결, Time: 604 ms, Memory: 49820 KB -BaekjoonHub

---
+++ 백준/Gold/1922. 네트워크 연결/네트워크 연결.java
@@ -0,0 +1,75 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.StringTokenizer;
+
+class Main {
+	public static int parent[];
+	public static void main(String args[]) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringTokenizer st;
+		ArrayList<Edge> list = new ArrayList<Edge>();
+		int N = Integer.parseInt(br.readLine());
+		int M = Integer.parseInt(br.readLine());
+		parent = new int[N + 1];
+		int s = 0;
+		
+		for (int i = 1; i <= N; i++) {
+			parent[i] = i;
+		}
+		
+		for (int i = 0; i < M; i++) {
+			st = new StringTokenizer(br.readLine());
+			list.add(new Edge(Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken())));
+		}
+
+		list.sort(new Comparator<Edge>() {
+            @Override
+            public int compare(Edge o1, Edge o2) {
+                return Integer.compare(o1.c, o2.c);
+            }
+        });
+		
+		for (int i = 0; i < M; i++) {
+			Edge temp = list.get(i);
+			if (!find(temp.s, temp.e)) {
+				union(temp.s, temp.e);
+				s += temp.c;
+			}
+		}
+		bw.write(String.valueOf(s));
+		bw.flush();
+		bw.close();
+	}
+	
+	public static int root(int i) {
+        while (i != parent[i]) {
+            parent[i] = parent[parent[i]];  
+            i = parent[i];
+        }
+        return i;
+    }
+
+    public static boolean find(int p, int q) {
+        return root(p) == root(q);
+    }
+
+    public static void union(int p, int q) {
+        int i = root(p);
+        int j = root(q);
+        parent[i] = j;
+    }
+}
+
+
+class Edge {
+	int s, e, c;
+	public Edge(int s, int e, int c) {
+		this.s = s; this.e = e; this.c = c;
+	}
+}
