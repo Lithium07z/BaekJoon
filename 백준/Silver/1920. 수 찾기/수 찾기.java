0b1b0fada2f5410d6eab278001b3be4b3507f3f6
Lithium <100747149+Lithium07z@users.noreply.github.com>
2023-09-04 20:33:05 +0900

    [Silver IV] Title: 수 찾기, Time: 572 ms, Memory: 49768 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.java
+++ 백준/Silver/1920. 수 찾기/수 찾기.java
@@ -3,33 +3,28 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
-import java.util.LinkedHashSet;
+import java.util.HashSet;
 import java.util.StringTokenizer;
 
 public class Main {
 	public static void main(String[] args) throws IOException {
-		BufferedReader bfr = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bfw = new BufferedWriter(new OutputStreamWriter(System.out));
- 		LinkedHashSet<String> set = new LinkedHashSet<String>();
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		HashSet<Long> set = new HashSet<Long>();
+		int N = Integer.parseInt(br.readLine());
 		
- 		int n = Integer.parseInt(bfr.readLine());
-		StringTokenizer st = new StringTokenizer(bfr.readLine());
-		
-		for (int i = 0; i < n; i++) {
-			set.add(st.nextToken());
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		for (int i = 0; i < N; i++) {
+			set.add(Long.parseLong(st.nextToken()));
+		}
+	
+		int M = Integer.parseInt(br.readLine());
+		st = new StringTokenizer(br.readLine());
+		for (int i = 0; i < M; i++) {
+			bw.write(set.contains(Long.parseLong(st.nextToken()))? "1\n" : "0\n");
 		}
 		
-		int m = Integer.parseInt(bfr.readLine());
-		st = new StringTokenizer(bfr.readLine());
-		
-		for (int i = 0; i < m; i++) {
-			if (set.add(st.nextToken()) == false) {
-				bfw.write("1\n");
-			} else {
-				bfw.write("0\n");
-			}
-		}
-		bfw.flush();
-		bfw.close();
+		bw.flush();
+		bw.close();
 	}
 }
