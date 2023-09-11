f6a864357f5df1c6cc7e181a6739687d80366f05
Lithium <100747149+Lithium07z@users.noreply.github.com>
2023-09-11 23:26:33 +0900

    [Gold V] Title: Bronze Cow Party, Time: 348 ms, Memory: 30144 KB -BaekjoonHub

---
+++ 백준/Gold/6248. Bronze Cow Party/Bronze Cow Party.java
@@ -0,0 +1,94 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
+
+public class Main {
+	static final int INF = 987654321;
+	static ArrayList<Node>[] graph;
+
+	public static void main(String args[]) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int N = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
+		int X = Integer.parseInt(st.nextToken()) - 1;
+		int max = Integer.MIN_VALUE;
+		graph = new ArrayList[N];
+		
+		for (int i = 0; i < N; i++) {
+			graph[i] = new ArrayList<Node>();
+		}
+
+		for (int i = 0; i < M; i++) {
+			st = new StringTokenizer(br.readLine());
+			int a = Integer.parseInt(st.nextToken()) - 1;
+			int b = Integer.parseInt(st.nextToken()) - 1;
+			int t = Integer.parseInt(st.nextToken());
+			
+			graph[a].add(new Node(b, t));
+			graph[b].add(new Node(a, t));
+		}
+
+		int dist[] = Dijkstra(X);
+		
+		for (int i = 0; i < N; i++) {
+			if (dist[i] != INF && dist[i] > max) {
+				max = dist[i];
+			}
+		}
+		
+		bw.write(max * 2 + "\n");
+		bw.flush();
+		bw.close();
+	}
+
+	public static int[] Dijkstra(int X) {
+		PriorityQueue<Node> pq = new PriorityQueue<Node>();
+		boolean visited[] = new boolean[graph.length];
+		int dist[] = new int[graph.length];
+		
+		Arrays.fill(dist, INF);
+		dist[X] = 0;
+		pq.offer(new Node(X, 0));
+		
+		while (!pq.isEmpty()) { 
+			int crt = pq.poll().idx;
+			
+			if (visited[crt]) {
+				continue;
+			}
+			visited[crt] = true;
+			
+			for (Node next : graph[crt]) {
+				if (dist[next.idx] > dist[crt] + next.v) {
+					dist[next.idx] = dist[crt] + next.v;
+					pq.offer(new Node(next.idx, dist[next.idx]));
+				}
+			}
+		}
+		
+		return dist;
+	}
+}
+
+class Node implements Comparable<Node> {
+	int idx;
+	int v;
+
+	public Node(int idx, int v) {
+		this.idx = idx;
+		this.v = v;
+	}
+
+	@Override
+	public int compareTo(Node o) {
+		return Integer.compare(this.v, o.v);
+	}
+}
