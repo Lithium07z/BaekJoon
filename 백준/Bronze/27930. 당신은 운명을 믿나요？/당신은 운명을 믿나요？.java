bf802dd1007d474263dc8fb11a0d6c5559f1edad
Lithium <100747149+Lithium07z@users.noreply.github.com>
2023-09-14 22:20:43 +0900

    [Bronze I] Title: 당신은 운명을 믿나요?, Time: 184 ms, Memory: 18652 KB -BaekjoonHub

--- 백준/Bronze/27930. 당신은 운명을 믿나요？/당신은 운명을 믿나요？.java
+++ 백준/Bronze/27930. 당신은 운명을 믿나요？/당신은 운명을 믿나요？.java
@@ -4,18 +4,32 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 
-public class Main {
+class Main {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		String N = br.readLine();
-		
-		if (N.contains("K") && N.contains("O") && N.contains("R") && N.contains("E") && N.contains("A")) {
-			bw.write("KOREA");
-		} else if (N.contains("Y") && N.contains("O") && N.contains("N") && N.contains("S") && N.contains("E") && N.contains("I")) {
-			bw.write("YONSEI");
+		String S = br.readLine();
+		String K = "KOREA";
+		String Y = "YONSEI";
+		int k = 0;
+		int y = 0;
+
+		for (int i = 0; i < S.length(); i++) {
+			if (k == 5) {
+				bw.write("KOREA");
+				break;
+			} else if (y == 6) {
+				bw.write("YONSEI");
+				break;
+			}
+
+			if (S.charAt(i) == K.charAt(k)) {
+				k++;
+			} else if (S.charAt(i) == Y.charAt(y)) {
+				y++;
+			}
 		}
-		
+
 		bw.flush();
 		bw.close();
 	}
