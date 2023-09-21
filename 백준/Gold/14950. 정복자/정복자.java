87eb596ec810f8d8f9059b487a17332bc93a40cb
Lithium <100747149+Lithium07z@users.noreply.github.com>
2023-09-21 15:30:46 +0900

    [Gold III] Title: 정복자, Time: 2252 ms, Memory: 28644 KB -BaekjoonHub

---
+++ 백준/Gold/14950. 정복자/정복자.java
@@ -0,0 +1,89 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.StringTokenizer;
+
+class Main {
+	public static int parent[];
+
+	public static void main(String args[]) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		ArrayList<Edge> list = new ArrayList<Edge>();
+		int N = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
+		int t = Integer.parseInt(st.nextToken());
+		int c = 0;
+		parent = new int[N + 1];
+		
+		for (int i = 1; i <= N; i++) {
+			parent[i] = i;
+		}
+
+		for (int i = 0; i < M; i++) {
+			st = new StringTokenizer(br.readLine());
+			list.add(new Edge(Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken())));
+		}
+
+		list.sort(new Comparator<Edge>() {
+			@Override
+			public int compare(Edge o1, Edge o2) {
+				return Integer.compare(o1.c, o2.c);
+			}
+		});
+
+		for (int i = 0; i < list.size(); i++) {
+			Edge temp = list.get(i);
+			if (!find(temp.s, temp.e)) {
+				union(temp.s, temp.e);
+				c += temp.c;
+				
+				for (int j = i + 1; j < list.size(); j++) {
+					list.get(j).c += t;
+				}
+			}
+		}
+
+		bw.write(c + "\n");
+		bw.flush();
+		bw.close();
+	}
+
+	public static int root(int i) {
+		while (i != parent[i]) {
+			parent[i] = parent[parent[i]];
+			i = parent[i];
+		}
+		return i;
+	}
+
+	public static boolean find(int p, int q) {
+		return root(p) == root(q);
+	}
+
+	public static void union(int p, int q) {
+		int i = root(p);
+		int j = root(q);
+
+		if (i > j) {
+			parent[i] = j;
+		} else {
+			parent[j] = i;
+		}
+	}
+}
+
+class Edge {
+	int s, e, c;
+
+	public Edge(int s, int e, int c) {
+		this.s = s;
+		this.e = e;
+		this.c = c;
+	}
+}
