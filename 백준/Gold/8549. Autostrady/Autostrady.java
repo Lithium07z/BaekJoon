77a6a348bdd585cb9f4b48844a06351eb1598211
Lithium <100747149+Lithium07z@users.noreply.github.com>
2023-09-26 19:32:51 +0900

    [Gold IV] Title: Autostrady, Time: 744 ms, Memory: 50708 KB -BaekjoonHub

---
+++ 백준/Gold/8549. Autostrady/Autostrady.java
@@ -0,0 +1,86 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.StringTokenizer;
+
+class Main {
+	public static int parent[];
+	public static ArrayList<Edge> list;
+
+	public static void main(String args[]) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int N = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
+		int c = Integer.MIN_VALUE;
+		parent = new int[N + 1];
+		list = new ArrayList<Edge>();
+
+		for (int i = 1; i <= N; i++) {
+			parent[i] = i;
+		}
+
+		for (int i = 0; i < M; i++) {
+			st = new StringTokenizer(br.readLine());
+			list.add(new Edge(Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken())));
+		}
+
+		list.sort(new Comparator<Edge>() {
+			@Override
+			public int compare(Edge o1, Edge o2) {
+				return o1.c - o2.c;
+			}
+		});
+		
+		for (int j = 0; j < list.size(); j++) {
+			Edge edge = list.get(j);
+
+			if (!find(edge.s, edge.e)) {
+				union(edge.s, edge.e);
+				c = Math.max(c, edge.c);
+			}
+		}
+		
+		bw.write(c + "\n");
+		bw.flush();
+		bw.close();
+	}
+
+	public static int root(int i) {
+		while (i != parent[i]) {
+			parent[i] = parent[parent[i]];
+			i = parent[i];
+		}
+		return i;
+	}
+
+	public static boolean find(int p, int q) {
+		return root(p) == root(q);
+	}
+
+	public static void union(int p, int q) {
+		int i = root(p);
+		int j = root(q);
+
+		if (i > j) {
+			parent[i] = j;
+		} else {
+			parent[j] = i;
+		}
+	}
+}
+
+class Edge {
+	int s, e, c;
+
+	public Edge(int s, int e, int c) {
+		this.s = s;
+		this.e = e;
+		this.c = c;
+	}
+}
