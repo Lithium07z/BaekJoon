4b881003cca990bc4fa9436e0f8e6657007d07f7
Lithium <100747149+Lithium07z@users.noreply.github.com>
2023-11-22 22:33:55 +0900

    [Gold V] Title: 감소하는 수, Time: 156 ms, Memory: 15804 KB -BaekjoonHub

---
+++ 백준/Gold/1038. 감소하는 수/감소하는 수.java
@@ -0,0 +1,46 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Collections;
+import java.util.LinkedList;
+
+class Main {
+	static LinkedList<Long> list = new LinkedList<Long>();
+	
+	public static void main(String args[]) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		int N = Integer.parseInt(br.readLine());
+		
+		if (N <= 10) {
+			bw.write(N + "\n");
+		} else if (N > 1022) {
+			bw.write("-1");
+		} else {
+			for (int i = 0; i < 10; i++) {
+				DFS(i, 1);
+			}
+			
+			Collections.sort(list);
+			
+			bw.write(list.get(N) + "\n");
+		}
+		
+		bw.flush();
+		bw.close();
+	}
+	
+	public static void DFS(long n, int idx) {
+		if (idx > 10) {
+			return;
+		}
+		
+		list.add(n);
+		
+		for (int i = 0; i < n % 10; i++) {
+			DFS((n * 10) + i, idx + 1);
+		}
+	}
+}
