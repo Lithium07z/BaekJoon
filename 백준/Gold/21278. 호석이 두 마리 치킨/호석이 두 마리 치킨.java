929342ef4efbcab65c7d1b552cff0b4848bbafe8
Lithium <100747149+Lithium07z@users.noreply.github.com>
2024-01-23 17:52:46 +0900

    [Gold V] Title: 호석이 두 마리 치킨, Time: 236 ms, Memory: 17620 KB, Score: 6 point -BaekjoonHub

---
+++ 백준/Gold/21278. 호석이 두 마리 치킨/호석이 두 마리 치킨.java
@@ -0,0 +1,62 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Main {
+	static int INF = 987654321;
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int N = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
+		int graph[][] = new int[N][N];
+		int result = Integer.MAX_VALUE;
+		int X = Integer.MAX_VALUE;
+		int Y = Integer.MAX_VALUE;
+		
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < N; j++) {
+				if (i != j) {
+					graph[i][j] = INF;
+				}
+			}
+		}
+
+		for (int i = 0; i < M; i++) {
+			st = new StringTokenizer(br.readLine());
+			int A = Integer.parseInt(st.nextToken()) - 1;
+			int B = Integer.parseInt(st.nextToken()) - 1;
+			graph[A][B] = graph[B][A] = 1;
+		}
+
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < N; j++) {
+				for (int k = 0; k < N; k++) {
+					graph[j][k] = Math.min(graph[j][k], graph[j][i] + graph[i][k]);
+				}
+			}
+		}
+		
+		for (int i = 0; i < N - 1; i++) {
+			int sum = 0;
+			
+			for (int j = i + 1; j < N; j++) {
+					for (int k = 0; k < N; k++) {
+						sum += Math.min(graph[i][k], graph[j][k]);
+					}
+					result = Math.min(result, sum);
+					X = Math.min(X, i);
+					Y = Math.min(Y, j);
+			}
+		}
+
+		bw.write((X + 1) + " " + (Y + 1) + " " + result * 2);
+		bw.flush();
+		bw.close();
+	}
+}
