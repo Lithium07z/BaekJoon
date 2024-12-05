a5263857c2db4d3bcfa458d6c9070a8517f23f8e
Lithium <100747149+Lithium07z@users.noreply.github.com>
2024-12-05 15:29:04 +0900

    [Gold IV] Title: 플로이드, Time: 20 ms, Memory: 2152 KB -BaekjoonHub

--- 백준/Gold/11404. 플로이드/플로이드.cc
+++ 백준/Gold/11404. 플로이드/플로이드.cc
@@ -1,70 +1,58 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
+int n, m, INF = 987654321;
+
 int main()
 {
-	ios::sync_with_stdio(0);
-	cin.tie(0);
-	cout.tie(0);
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
 
-	int n, m, INF = 987654321;
 	cin >> n >> m;
 
-	int **arr = new int *[n + 1];
+	vector<vector<int>> arr(n + 1, vector<int>(n + 1, 0));
 
-	for (int i = 1; i <= n; i++)
+	for (int i = 1; i <= n; ++i)
 	{
-		arr[i] = new int[n + 1];
-		for (int j = 1; j <= n; j++)
+		for (int j = 1; j <= n; ++j)
 		{
-			if (i == j)
-			{
-				arr[i][j] = 0;
-			}
-			else
+			if (i != j)
 			{
 				arr[i][j] = INF;
 			}
 		}
 	}
 
-	for (int i = 0; i < m; i++)
+	for (int i = 0; i < m; ++i)
 	{
 		int a, b, c;
 		cin >> a >> b >> c;
-		if (arr[a][b] != 0 && arr[a][b] > c)
+
+		if (arr[a][b] > c)
 		{
 			arr[a][b] = c;
 		}
 	}
 
-	for (int i = 1; i <= n; i++)
+	for (int i = 1; i <= n; ++i)
 	{
-		for (int j = 1; j <= n; j++)
+		for (int j = 1; j <= n; ++j)
 		{
-			for (int k = 1; k <= n; k++)
+			for (int k = 1; k <= n; ++k)
 			{
-				if (arr[j][k] > arr[j][i] + arr[i][k])
-				{
-					arr[j][k] = arr[j][i] + arr[i][k];
-				}
+                arr[j][k] = min(arr[j][k], arr[j][i] + arr[i][k]);
 			}
 		}
 	}
 
-	for (int i = 1; i <= n; i++)
+	for (int i = 1; i <= n; ++i)
 	{
-		for (int j = 1; j <= n; j++)
+		for (int j = 1; j <= n; ++j)
 		{
-			if (arr[i][j] == INF)
-			{
-				cout << "0 ";
-			}
-			else
-			{
-				cout << arr[i][j] << " ";
-			}
+            cout << ((arr[i][j] == INF) ? 0 : arr[i][j]) << " ";
 		}
 		cout << "\n";
 	}
